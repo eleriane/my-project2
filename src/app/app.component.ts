4347f0f79ad621c6040e0fab9443bf19f1800dda
eleriane <eleriane@gmail.com>
2017-11-27 16:06:08 -0200

    Components and Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Customer App';
+  name = 'Bob';
+  wardsColor = 'blue';
+  customers = [
+    { id: 1, name: 'Ward' },
+  ];
+
+  changeSuitColor() {
+    this.wardsColor = this.wardsColor === 'blue' ? 'red' : 'blue';
+  }
+}
+
